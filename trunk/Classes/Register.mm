a8487e2ec61adf5cfaea525f3d3b07098b0cd202
ar.bartle <ar.bartle@3556a761-becf-d89a-4382-d1216a525bb9>
2011-03-23 12:37:50 +0000

    classes Register & Tetrad

---
+++ trunk/Classes/Register.mm
@@ -0,0 +1,17 @@
+//
+//  Register.m
+//  mt1804
+//
+//  Created by bartle on 3/23/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include "Register.h"
+
+Register::Register()
+{
+}
+
+Register::~Register()
+{
+}
---
+++ trunk/Classes/Tetrad.h
@@ -0,0 +1,18 @@
+//
+//  Tetrad.h
+//  mt1804
+//
+//  Created by bartle on 3/23/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+class Tetrad
+{
+	bool t0;
+	bool t1;
+	bool t2;
+	bool t3;
+public:
+	Tetrad();
+	~Tetrad();
+};
